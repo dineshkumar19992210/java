74085e3897f33c5d70099cd18bb5076a1f98ae1c
dineshkumar19992210 <147531904+dineshkumar19992210@users.noreply.github.com>
2023-10-10 21:20:36 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,14 @@
+class dowhile
+{
+	public static void main(String[] args)
+	{
+		int a=1289;
+		do
+		{
+			System.out.println("tamil");
+			a++;
+		}
+		while(a<=5);
+	}
+}
+	
---
+++ program1.java
@@ -0,0 +1,10 @@
+class program1
+{
+ public static void main(String[] agrs)
+ {
+	 System.out.println(false&&false);
+	 System.out.println(false&&true);
+	 System.out.println(true&&false);
+	 System.out.println(true&&true);
+ }
+}
